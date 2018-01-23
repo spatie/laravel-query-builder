99afd17f86a03e1437d0b8f049c194cfd72560f2
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-01-23 12:13:33 +0100

    Revert "Escape LIKE queries (#11)"
    This reverts commit 5cfece7aaf341dede3b3a9b1abf7a4749d7c58c5.

--- src/Filters/FiltersPartial.php
+++ src/Filters/FiltersPartial.php
@@ -2,7 +2,6 @@
 
 namespace Spatie\QueryBuilder\Filters;
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Builder;
 
 class FiltersPartial implements Filter
@@ -12,20 +11,11 @@ class FiltersPartial implements Filter
         if (is_array($value)) {
             return $query->where(function (Builder $query) use ($value, $property) {
                 foreach ($value as $partialValue) {
-                    $query
-                        ->orWhere($property, 'LIKE', DB::raw('? ESCAPE "\"'))
-                        ->addBinding("%{$this->escapeLike($partialValue)}%");
+                    $query->orWhere($property, 'LIKE', "%{$partialValue}%");
                 }
             });
         }
 
-        return $query
-            ->where($property, 'LIKE', DB::raw('? ESCAPE "\"'))
-            ->addBinding("%{$this->escapeLike($value)}%");
-    }
-
-    protected function escapeLike(string $value): string
-    {
-        return addcslashes($value, '\%_');
+        return $query->where($property, 'LIKE', "%{$value}%");
     }
 }
