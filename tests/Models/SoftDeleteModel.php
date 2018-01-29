a3472feb57270a8ffe9bb0b9be5e9b2fc7cd9268
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-01-29 10:18:15 +0000

    Apply fixes from StyleCI

--- tests/Models/SoftDeleteModel.php
+++ tests/Models/SoftDeleteModel.php
@@ -3,7 +3,6 @@
 namespace Spatie\QueryBuilder\Tests\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SoftDeleteModel extends Model
