00b6288a14862882e4d162fbcc4aa2e2afed46b8
cornelp <cornelp@users.noreply.github.com>
2018-03-05 18:06:48 +0200

    Add support for selecting fields/columns. (#35)
    * Added support for selecting columns.
    
    * Changes according to sparse fieldset recommandations.
    
    * Fields are loaded correctly for nested relations.

---
+++ tests/ColumnTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Spatie\QueryBuilder\Tests;
+
+use Spatie\QueryBuilder\Tests\Models\TestModel;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+
+
+class ColumnTest extends TestCase
+{
+    protected $model;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->model = factory(TestModel::class)->create();
+    }
+
+    /** @test */
+    public function it_can_fetch_all_columns_if_none_is_given()
+    {
+        $queryBuilder = QueryBuilder::for(TestModel::class)->toSql();
+
+        $expected = TestModel::query()->toSql();
+
+        $this->assertEquals($expected, $queryBuilder);
+    }
+
+    /** @test */
+    public function it_can_fetch_only_required_columns()
+    {
+        $request = new Request([
+            'fields' => ['test_models' => 'name']
+        ]);
+
+
+        $queryBuilder = QueryBuilder::for(TestModel::class, $request)->toSql();
+
+        $expected = TestModel::query()->select('name')->toSql();
+
+        $this->assertEquals($expected, $queryBuilder);
+    }
+}
