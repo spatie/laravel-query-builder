5863a95c0995bc1089947657aab7e76bda1e10ac
Bruno Tomé <ibrunotome@gmail.com>
2018-06-04 09:28:26 -0300

    Resolve #14 (query with case-insensitive)

--- src/Filters/FiltersPartial.php
+++ src/Filters/FiltersPartial.php
@@ -6,16 +6,22 @@ use Illuminate\Database\Eloquent\Builder;
 
 class FiltersPartial implements Filter
 {
-    public function __invoke(Builder $query, $value, string $property) : Builder
+    public function __invoke(Builder $query, $value, string $property): Builder
     {
+        $sql = "LOWER({$property}) LIKE ?";
+
         if (is_array($value)) {
-            return $query->where(function (Builder $query) use ($value, $property) {
+            return $query->where(function (Builder $query) use ($value, $sql) {
                 foreach ($value as $partialValue) {
-                    $query->orWhere($property, 'LIKE', "%{$partialValue}%");
+                    $partialValue = strtolower($partialValue);
+
+                    $query->orWhereRaw($sql, ["%{$partialValue}%"]);
                 }
             });
         }
 
-        return $query->where($property, 'LIKE', "%{$value}%");
+        $value = strtolower($value);
+
+        return $query->whereRaw($sql, ["%{$value}%"]);
     }
 }
