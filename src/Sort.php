ca8c7b971e3ff2c0f3e5068f425f08a1bd9b8b8e
Alex Vanderbist <alex.vanderbist@gmail.com>
2019-01-16 09:38:52 +0000

    Apply fixes from StyleCI

--- src/Sort.php
+++ src/Sort.php
@@ -3,8 +3,8 @@
 namespace Spatie\QueryBuilder;
 
 use Illuminate\Database\Eloquent\Builder;
-use Spatie\QueryBuilder\Enums\SortDirection;
 use Spatie\QueryBuilder\Sorts\SortsField;
+use Spatie\QueryBuilder\Enums\SortDirection;
 use Spatie\QueryBuilder\Sorts\Sort as CustomSort;
 
 class Sort
