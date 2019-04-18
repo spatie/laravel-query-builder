1431c7c71fb324d2e197b21aa73619be670d39d1
Alex Vanderbist <alex.vanderbist@gmail.com>
2019-04-18 08:56:29 +0000

    Apply fixes from StyleCI

--- tests/Models/TestModel.php
+++ tests/Models/TestModel.php
@@ -2,11 +2,11 @@
 
 namespace Spatie\QueryBuilder\Tests\Models;
 
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
