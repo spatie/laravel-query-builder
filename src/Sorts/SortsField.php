dd7fc645195b79eb4f94c3ca752614eac06c032f
Arjan <arjanwestdorp@gmail.com>
2019-08-12 15:18:00 +0200

    Fix missing typehint in SortsField

--- src/Sorts/SortsField.php
+++ src/Sorts/SortsField.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 
 class SortsField implements Sort
 {
-    public function __invoke(Builder $query, $descending, string $property)
+    public function __invoke(Builder $query, bool $descending, string $property)
     {
         $query->orderBy($property, $descending ? 'desc' : 'asc');
     }
