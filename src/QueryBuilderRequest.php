63ff225d169f713cf39dc10b422aba0929ac68ae
Paige Jones <hello@paigejones.me>
2020-01-06 14:03:43 +0000

    remove setting the appends to lowercase

--- src/QueryBuilderRequest.php
+++ src/QueryBuilderRequest.php
@@ -35,7 +35,7 @@ class QueryBuilderRequest extends Request
         $appendParts = $this->query($appendParameterName);
 
         if (! is_array($appendParts)) {
-            $appendParts = explode(',', strtolower($appendParts));
+            $appendParts = explode(',', $appendParts);
         }
 
         return collect($appendParts)->filter();
