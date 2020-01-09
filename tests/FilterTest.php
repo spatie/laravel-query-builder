08dec9f42dd9e0a6c04c4f82bdc8ec40c81bc074
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-01-09 11:02:24 +0100

    Fix test false positive due to faker

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -435,14 +435,14 @@ class FilterTest extends TestCase
     /** @test */
     public function it_does_not_apply_default_filter_when_filter_exists_and_default_is_set()
     {
-        TestModel::create(['name' => 'John Doe']);
-        TestModel::create(['name' => 'John Deer']);
+        TestModel::create(['name' => 'UniqueJohn UniqueDoe']);
+        TestModel::create(['name' => 'UniqueJohn Deer']);
 
         $models = $this
             ->createQueryFromFilterRequest([
-                'name' => 'Doe',
+                'name' => 'UniqueDoe',
             ])
-            ->allowedFilters(AllowedFilter::partial('name')->default('John'))
+            ->allowedFilters(AllowedFilter::partial('name')->default('UniqueJohn'))
             ->get();
 
         $this->assertEquals(1, $models->count());
