7d0035df45bff9bc4e7b70dd2e110d820795e3d6
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-03-10 12:29:53 +0100

    Re-enable relationship query test

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -221,44 +221,19 @@ class QueryBuilderTest extends TestCase
     }
 
     /** @test */
-    public function it_can_get_a_query_from_a_relationship()
-    {
-        $this->markTestIncomplete('This test proves that there\'s something wrong with Eloquent\'s getQuery()');
-
-        $testModel = TestModel::create(['id' => 321, 'name' => 'John Doe']);
-        $relatedThroughPivotModel = RelatedThroughPivotModel::create(['id' => 789, 'name' => 'The related model']);
-
-        $testModel->relatedThroughPivotModels()->attach($relatedThroughPivotModel);
-
-        // Passes
-        $this->assertEquals(
-            $relatedThroughPivotModel->id,
-            $testModel->relatedThroughPivotModels()->first()->id
-        );
-
-        // Fails
-        $this->assertEquals(
-            $relatedThroughPivotModel->id,
-            $testModel->relatedThroughPivotModels()->getQuery()->first()->id
-        );
-    }
-
-    /** @test */
     public function it_queries_the_correct_data_for_a_relationship_query()
     {
-        $this->markTestIncomplete('Currently broken due to Eloquent. See the above test.');
-
         $testModel = TestModel::create(['id' => 321, 'name' => 'John Doe']);
         $relatedThroughPivotModel = RelatedThroughPivotModel::create(['id' => 789, 'name' => 'The related model']);
 
         $testModel->relatedThroughPivotModels()->attach($relatedThroughPivotModel);
 
-        $baseQuery = $testModel->relatedThroughPivotModels()->getQuery();
+        $relationship = $testModel->relatedThroughPivotModels()->with('testModels');
 
-        $queryBuilderResult = QueryBuilder::for($baseQuery)->first();
+        $queryBuilderResult = QueryBuilder::for($relationship)->first();
 
         $this->assertEquals(789, $queryBuilderResult->id);
-        $this->assertEquals(123, $queryBuilderResult->testModel->id);
+        $this->assertEquals(321, $queryBuilderResult->testModels->first()->id);
     }
 
     /** @test */
