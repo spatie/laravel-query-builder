9677917c91e0ab53d0d310e6e0696cbe959bcde1
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-03-20 09:36:41 +0000

    Apply fixes from StyleCI

--- src/QueryBuilderServiceProvider.php
+++ src/QueryBuilderServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace Spatie\QueryBuilder;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
 
 class QueryBuilderServiceProvider extends ServiceProvider implements DeferrableProvider
 {
