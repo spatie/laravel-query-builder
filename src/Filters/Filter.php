14c1d51bb76aa0195dcd3248ed3f6e6edf4c6464
Kay Wei <weikaiii@sina.cn>
2022-04-24 15:33:58 +0800

    Fix Laravel 9 PHPStan generic check for Builder in `__invoke() of Filter`

--- src/Filters/Filter.php
+++ src/Filters/Filter.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Builder;
 
 interface Filter
 {
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder<\Illuminate\Database\Eloquent\Model> $query
+     * @param mixed $value
+     * @param string $property
+     *
+     * @return mixed
+     */
     public function __invoke(Builder $query, $value, string $property);
 }
