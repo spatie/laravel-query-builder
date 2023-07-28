8f4e9224af154aca0b4a82e13f4e5bdf0f571c6e
Birtan Taşkın <birtan.taskin@tsoft.com.tr>
2023-07-28 18:17:45 +0300

    Laravel Spatie Query Builder - create OR condition for filters / Tests/FilterTest is-resolved

--- src/Filters/FiltersSearch.php
+++ src/Filters/FiltersSearch.php
@@ -13,9 +13,16 @@ class FiltersSearch implements Filter
     /** {@inheritdoc} */
     public function __invoke(Builder $query, $values, string $property)
     {
+
         return $query->where(function ($q) use ($values){
             foreach($values as $item):
-                $q->orWhere($item['column'], 'LIKE', '%' . $item['value'] . '%');
+                if(is_array($item['value'])) {
+                    foreach($item['value'] as $invalue):
+                        $q->orWhere($item['column'], 'LIKE', '%' . $invalue . '%');
+                    endforeach;
+                } else {
+                    $q->orWhere($item['column'], 'LIKE', '%' . $item['value'] . '%');
+                }
             endforeach;
         });
     }
