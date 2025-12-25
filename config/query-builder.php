c8caba874d4073671c3b0f81e6503267fb807beb
Shaid <shaidt137@gmail.com>
2025-12-25 01:45:22 -0600

    Update query-builder.php

--- config/query-builder.php
+++ config/query-builder.php
@@ -2,12 +2,7 @@
 
 return [
 
-    /*
-     * By default the package will use the `include`, `filter`, `sort`
-     * and `fields` query parameters as described in the readme.
-     *
-     * You can customize these query string parameters here.
-     */
+
     'parameters' => [
         'include' => 'include',
 
