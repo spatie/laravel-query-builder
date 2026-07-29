ec365b5f85a7b21089f0fa5112080d0cd0d467d4
Hossein Hosni <hosni.hossein@gmail.com>
2026-07-29 11:46:40 +0330

    fix(phpstan): define paarmeter types of InvalidSortQuery exception

--- src/Exceptions/InvalidSortQuery.php
+++ src/Exceptions/InvalidSortQuery.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Collection;
 
 class InvalidSortQuery extends InvalidQuery
 {
+    /**
+     * @param Collection<int,non-empty-string> $unknownSorts
+     * @param Collection<int,non-empty-string> $allowedSorts
+     */
     public function __construct(
         public Collection $unknownSorts,
         public Collection $allowedSorts
@@ -19,6 +23,10 @@ class InvalidSortQuery extends InvalidQuery
         parent::__construct(Response::HTTP_BAD_REQUEST, $message);
     }
 
+    /**
+     * @param Collection<int,non-empty-string> $unknownSorts
+     * @param Collection<int,non-empty-string> $allowedSorts
+     */
     public static function sortsNotAllowed(Collection $unknownSorts, Collection $allowedSorts): static
     {
         return new static(...func_get_args());
